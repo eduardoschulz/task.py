3a966c169efdeffbb4ebbbcdbe588360a4170e53
Eduardo Schulz <eduardo.schulz@protonmail.com>
2025-06-28 00:47:05 -0300

    added a way to open the menu

--- task_py/app.py
+++ task_py/app.py
@@ -1,30 +1,39 @@
+from textual import on
 from textual.app import App
 from textual.containers import Vertical, Middle, Container
 from task_py.components.addtask import AddTask
 from task_py.components.task import TaskWidget
 from textual.widgets import Footer
+from textual.events import Focus
 
 # uso futuro from task_py.components.task import Task
+
 
 
 class TodoApp(App):
     CSS_PATH = "styles/app.tcss"
     BINDINGS = [
-        ("o", "open_menu", "Open/Close Menu") # Added this line
+        ("a", "open_menu", "Open/Close Menu") # Added this line
     ]
+
     def compose(self):
         yield AddTask(id="addtask", classes="hidden")
         yield Vertical(
-            TaskWidget(), TaskWidget(), TaskWidget(), TaskWidget(),
-            id="task-list"
-        )
+             TaskWidget(), TaskWidget(), TaskWidget(), TaskWidget(),
+             id="task-list",
+             classes=""
+         )
 
     #TODO descobrir pq nao aparece/ ele ta chegando aq
     def action_open_menu(self): 
+
+        tasklist = self.query_one("#task-list")
+        tasklist.add_class("hidden")
         menu = self.query_one("#addtask")
         menu.remove_class("hidden")
-        self.bell()
+    
+    # @on(Button.Pressed, "#input-add")
+    # def setdone(self):
+    #     self.query_one("#task-list")
 
-    def on_mount(self) -> None:
-        self.query_one(Vertical).focus()
-
+   
--- task_py/components/addtask.py
+++ task_py/components/addtask.py
@@ -1,7 +1,17 @@
 from textual.widgets import TextArea, Static, Input, Button
+from textual import on
 
 
 class AddTask(Static):
     def compose(self):
         yield Input(placeholder="Nova Tarefa", id="input-add")
         yield Button("Add Task", id="add-task")
+
+    @on(Button.Pressed, '#add-task')
+    def set_as_done(self):
+        self.add_class("hidden")
+        # menu = self.query_one("#addtask")
+        # menu.add_class("hidden")
+        # tl = self.query_one("#task-list")
+        # tl.remove_class("hidden")
+
